9055af4f6feee075282723e7833a0cb3a3ff18df
Pat Migliaccio <pat@oppie.io>
2020-06-02 21:42:42 -0700

    add regex to check for existing task name to avoid adding duplicate prefixes add optional WATCH_CHANGES env variable for updating task names on task changed events

--- services/Asana.ts
+++ services/Asana.ts
@@ -1,7 +1,7 @@
 import { AsanaEvent } from "../interfaces/Asana";
 import * as Asana from "asana";
 
-const TASK_ID_PATTERN = /\[(.+?)\]/;
+const TASK_PREFIX_PATTERN = /\[(.+?)\]/;
 
 const registerClient = (): Asana.Client => {
   return Asana.Client.create().useAccessToken(process.env.ASANA_ACCESS_TOKEN);
@@ -15,35 +15,40 @@ export const createHook = async (projectId: string, url: string) => {
 };
 
 export const handleHook = async (body: { events?: AsanaEvent[] }) => {
-  const isAddedEvent = (x: AsanaEvent) => x.action === "added";
+  const isWatchChangeEvents = process.env.WATCH_CHANGES === "true";
+  const isActiveEvent = (x: AsanaEvent) => x.action === "added" || (isWatchChangeEvents && x.action === "changed");
   const filterTaskResource = (x: AsanaEvent) =>
-    x.resource.resource_type === "task" && x.parent.resource_type === "project";
+    x.resource.resource_type === "task" && (!x.parent || x.parent.resource_type === "project");
 
-  const addedTasks = body && body.events ? body.events
-    .filter(isAddedEvent)
+  const activeTasks = body && body.events ? body.events
+    .filter(isActiveEvent)
     .filter(filterTaskResource) : [];
 
-  if (addedTasks && addedTasks.length) {
+  if (activeTasks && activeTasks.length) {
     const client = registerClient();
     const project = await client.projects.findById(process.env.ASANA_PROJECT_ID);
     const currentId = await getProjectCurrentId(project);
 
     let updatedId = currentId;
-    for (let task of addedTasks) {
-      updatedId++;
-      const name = await createUpdatedTaskName(client, task, updatedId);
-      await client.tasks.update(task.resource.gid, { name });
-      console.log(`Task updated: '${name}'`);
+    for (let task of activeTasks) {
+      const currentTask = await client.tasks.findById(task.resource.gid);
+      const isTaskPrefixAlreadyExists = !!currentTask.name.match(TASK_PREFIX_PATTERN);
+      if (!isTaskPrefixAlreadyExists) {
+        updatedId++;
+        const name = createUpdatedTaskName(currentTask, updatedId);
+        await client.tasks.update(task.resource.gid, { name });
+        console.log(`Task updated: '${name}'`);
+      }
     }
 
-    await setProjectCurrentId(client, updatedId);
+    if (updatedId > currentId) await setProjectCurrentId(client, updatedId);
   }
 };
 
 export const getProjectCurrentId = async (
   project: Asana.resources.Projects.Type
 ): Promise<number> => {
-  const match = project.notes.match(TASK_ID_PATTERN);
+  const match = project.notes.match(TASK_PREFIX_PATTERN);
   return match ? parseInt(match[1], 10) : 0;
 };
 
@@ -56,12 +61,10 @@ export const setProjectCurrentId = async (
   await client.projects.update(process.env.ASANA_PROJECT_ID, { notes });
 };
 
-export const createUpdatedTaskName = async (
-  client: Asana.Client,
-  task: AsanaEvent,
+export const createUpdatedTaskName = (
+  currentTask: Asana.resources.Tasks.Type,
   updatedId: number,
-): Promise<string> => {
-  const currentTask = await client.tasks.findById(task.resource.gid);
+): string => {
   const taskPrefix = `[${process.env.ASANA_PROJECT_PREFIX}-${updatedId}]`;
   return `${taskPrefix} ${currentTask.name}`;
 };
